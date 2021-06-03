ecbd254f7e06b7e0bb4bfc11f9ec6b721f715251
rosreekunar-9908 <84075065+rosreekunar-9908@users.noreply.github.com>
2021-06-03 07:45:07 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -39,5 +39,24 @@ int main(void) {
   }
 }
 
+float mc_pi(int n)
+{
+  int i,count;
+  double x,y,z,pi;
+  count=0;
+  for (i=0;i<n;i++)
+  {
+    x=(double)rand()/ RAND_MAX;
+    y=(double)rand()/ RAND_MAX;
+    z=x*x+y*y;
+    if(z<=1)
+      count++;
+  }
+  pi=(double)count/n*4;
+  return pi;
+}
+
+  
+
 
 
